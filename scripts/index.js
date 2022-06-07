ec7a7cbfa8a6d9fe165ec592a9da6c28dcbfa8d3
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-06-07 14:20:00 +0100

    Now renders shades and changes on new seed pick or mode

--- scripts/index.js
+++ scripts/index.js
@@ -3,23 +3,52 @@ document.addEventListener('DOMContentLoaded', () => {
     const colourPicker = document.getElementById('seed-picker')
     const shades = document.querySelectorAll(".colour-shade");
     const modeOptions = document.getElementById('mode-list');
+    const baseUrl = "https://www.thecolorapi.com/scheme";
 
     let currentColour = colourPicker.value;
+    let currentMode = '';
+    
+    colourPicker.addEventListener('change', (event) => {
+        
+        currentColour = event.target.value;
+        shade1.style.backgroundColor = currentColour;
+        getColours(currentColour.substring(1), currentMode);
+    })
 
-        colourPicker.addEventListener('change', (event) => {
-            
-            currentColour = event.target.value;
-            console.log(currentColour);
-            shade1.style.backgroundColor = currentColour;
-        })
+    modeOptions.addEventListener('change', (event) => {
+        currentMode = event.target.value;
+        console.log(currentColour)
+        getColours(currentColour,currentMode)
+    })
+
+    function renderPage() {
+        loadList();
+        getColours(currentColour.substring(1), currentMode);
+    }
     
     function loadList() {
-        schemeModes.map(mode => {
+        schemeModes.forEach(mode => {
             const option = document.createElement('option');
-            console.log(option)
             option.textContent = mode;
+            option.value = mode.toLowerCase();
             modeOptions.appendChild(option);
-        })        
+        }) 
+        currentMode = modeOptions.options[modeOptions.selectedIndex].value;
     }
-    loadList();
+
+    function loadShades(shadesArr) {
+        shades.forEach((shade, index) => {
+            shade.style.backgroundColor = shadesArr[index].hex.value;
+        })
+    }
+
+    async function getColours(hexValue, mode) {
+        const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}&mode=${mode}&count=${shades.length}`)
+        const { colors } = await response.json()
+        console.log(colors)
+        loadShades(colors);
+    }
+
+    renderPage();
+    
 })
