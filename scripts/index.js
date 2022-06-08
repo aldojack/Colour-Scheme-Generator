7e6d133546370723418433150338701aac06d766
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-06-08 10:54:59 +0100

    divs and shade count added dynamically

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     const schemeModes = ['Monochrome', 'Monochrome-Dark', 'Monochrome-Light', 'Analogic', 'Complement', 'Analogic-Complement', 'Triad', 'Quad']
     const colourPicker = document.getElementById('seed-picker')
-    const shades = document.querySelectorAll(".colour-shade");
     const modeOptions = document.getElementById('mode-list');
+    
     const baseUrl = "https://www.thecolorapi.com/scheme";
-
+    const shadesCount = 5;
     let currentColour = colourPicker.value;
     let currentMode = '';
+    let shades = '';
+    const year = document.querySelector('span');
+    year.textContent = new Date().getFullYear();
     
     colourPicker.addEventListener('change', (event) => {
         
         currentColour = event.target.value;
-        shade1.style.backgroundColor = currentColour;
+        console.log("Seed picker: ",currentColour, currentMode)
         getColours(currentColour.substring(1), currentMode);
     })
 
     modeOptions.addEventListener('change', (event) => {
         currentMode = event.target.value;
-        console.log(currentColour)
-        getColours(currentColour,currentMode)
+        console.log("Mode: ", currentColour, currentMode)
+        getColours(currentColour.substring(1), currentMode);
     })
 
     function renderPage() {
         loadList();
+        loadDivs();
         getColours(currentColour.substring(1), currentMode);
     }
     
@@ -36,14 +40,31 @@ document.addEventListener('DOMContentLoaded', () => {
         currentMode = modeOptions.options[modeOptions.selectedIndex].value;
     }
 
+    function loadDivs() {
+        const shadeContainer = document.getElementById('shades-container');
+
+        for (let i = 0; i < shadesCount; i++) {
+            const div = document.createElement('div');
+            div.classList.add('colour-shade');
+            div.addEventListener('click', event => {
+                const value = div.textContent;
+                navigator.clipboard.writeText(value);
+                alert(`${value} copied to clipboard`)
+            })
+            shadeContainer.appendChild(div);
+        }
+        shades = document.querySelectorAll(".colour-shade")
+    }
+
     function loadShades(shadesArr) {
         shades.forEach((shade, index) => {
             shade.style.backgroundColor = shadesArr[index].hex.value;
+            shade.textContent = shadesArr[index].hex.value;
         })
     }
 
     async function getColours(hexValue, mode) {
-        const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}&mode=${mode}&count=${shades.length}`)
+        const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}&mode=${mode}&count=${shadesCount}`)
         const { colors } = await response.json()
         console.log(colors)
         loadShades(colors);
